56cac2e2725050cd8cb27a11305389eab35612f9
Vishal <vishaldesktop67@gmail.com>
2025-08-29 11:51:04 +0530

    Add files via upload

---
+++ VISHAL CSE-D(2025-2029)/Q2.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+	int i;
+	scanf("%s",i);
+	printf("%s",i);
+	return 0;
+}
---
+++ VISHAL CSE-D(2025-2029)/Q9.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+	int i;
+	scanf("%d",&i);
+	printf("%.6d",i);
+	return 0;
+}
